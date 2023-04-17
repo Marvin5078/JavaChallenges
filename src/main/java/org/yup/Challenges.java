80a4ffab52805a79ac666caa1936e21d64eac7cf
Marvin Oreste <marvin5078@gmail.com>
2023-04-17 16:01:30 -0400

    second commit

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,6 +1,89 @@
 package org.yup;
 
+import java.util.Scanner;
+
 public class Challenges {
 
-    
+    public String greetingWithUserInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //we need to ask the user what their name is?
+        System.out.print("What is your name: ");
+        String name = scanner.nextLine();
+
+
+        //we need to ask the user what their age is?
+        System.out.print("Enter your Age: ");
+        int age = scanner.nextInt();
+
+        //control flow logic (is an if statement sort of like protocols)
+        if (age >= 21) {
+            return "No problem " + name + " you can get in";
+        } else {
+            return "Sorry " + name + " but no babies allowed.";
+        }
+
+    }
+
+    public String greeting(String name, int age) {
+
+        //control flow logic (is an if statement sort of like protocols)
+        if (age >= 21) {
+            return "No problem " + name + " you can get in";
+        } else {
+            return "Sorry " + name + " but no babies allowed.";
+        }
+
+    }
+
+    public int addTwoNumbers() {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        //ask the user for number
+        System.out.print("Enter first number to add: ");
+        int number = scan.nextInt();
+
+        //ask the user for number2
+        System.out.print("Enter second number to add: ");
+        int number2 = scan.nextInt();
+
+
+        //return the sum of number1 and number 2 together.
+        return number + number2;
+    }
+
+
+    public int subtractTwoNumbers() {
+        // create a method that subtracts 2 numbers and multiply 2 numbers
+        Scanner scan = new Scanner(System.in);
+
+
+        //ask the user for number
+        System.out.print("Enter first number to subtract: ");
+        int number = scan.nextInt();
+
+
+        //ask the user for number2
+        System.out.print("Enter second number to subtract: ");
+        int number2 = scan.nextInt();
+            return number - number2;
+
+    }
+
+
+    public int multiplyTwoNumbers() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter first number to multiply: ");
+        int number1 = scan.nextInt();
+
+        System.out.print("Enter second number to multiply: ");
+        int number2 = scan.nextInt();
+
+        return number1 * number2;
+    }
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,9 +1,59 @@
 package org.yup;
-
+import org.yup.Truth;
 public class JavaChallengesApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from the other side!");
+
+
+        //this code is running greeting
+        //creates a new instance of the challenges class, so we can use the challenges in our application.
+        Challenges challenges = new Challenges();
+
+        /*
+
+        //this runs the greeting method from our challenges and returns a string greeting and
+        //stores it in the greeting variable we created.
+        String greeting = challenges.greeting("marvin",21);
+
+        System.out.println(greeting);
+
+
+        //this code is running love
+
+        Truth truth = new Truth(); //we defined the empty concept word 'truth' with what we defined 'Truth' to be.
+
+        String love = truth.Love("Who loves me?", 1); //we defined the empty concept word 'love'
+                                                                    //with what we defined 'Love' to be.
+
+        System.out.println(love);
+
+        //this code gives love a choice
+        String choice = truth.ChoicesOfLove(); //we just defined the empty word 'choice' with what we defined
+                                                //'ChoicesOfLove to be.
+
+        System.out.println(choice);
+
+        //greeting with userInput
+        String greetingWithUI = challenges.greetingWithUserInput();
+
+        System.out.println(greetingWithUI); */
+
+        //this code should do addition math
+        int add = challenges.addTwoNumbers();
+
+        int subtract = challenges.subtractTwoNumbers();
+
+        int multiply = challenges.multiplyTwoNumbers();
+
+        System.out.print(add);
+
+        System.out.print(subtract);
+
+        System.out.print(multiply);
+
+
+
+
     }
 }
